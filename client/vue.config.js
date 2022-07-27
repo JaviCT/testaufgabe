9e8c12bc068e41d9dbe266c264db8813f0ff15e2
javier calero <javier@evercam.io>
2022-07-27 11:45:45 +0200

    Fix build on dev mode

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  productionSourceMap: false,  
-  outputDir: '../dist',
-  assetsDir: 'static',
-  indexPath: 'templates/index.html'
-})
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports = defineConfig({
+    transpileDependencies: true,
+  })
+} else {
+  module.exports = defineConfig({
+    transpileDependencies: true,
+    productionSourceMap: false,  
+    outputDir: '../dist',
+    assetsDir: 'static',
+    indexPath: 'templates/index.html'
+  })
+}
