77c48477b3fe470570a21787d0e4d5d9aebae08a
javier calero <javier@evercam.io>
2022-07-27 11:42:00 +0200

    Add OpenAPI

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from cmath import log
 from flask import Flask, render_template
 from flask_cors import CORS
+from flasgger import Swagger
 import pandas as pd
 import numpy as np
 import json
@@ -7,11 +9,15 @@ import requests
 
 app = Flask(__name__, static_folder="./dist/static",
             template_folder="./dist/templates")
+swagger = Swagger(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route("/data")
 def get_df():
+    """
+    file: server/swagger/data.yml
+    """
     random_data = np.random.randint(0, 100, size=1000)
     mod = np.array([s % 10 for s in random_data])
 
